278a05362c75f8d0d76946213042657f8e863672
홍준성 <powerstar13@hanmail.net>
2022-04-22 13:12:37 +0900

    git init > MSA 설계 과제 > README 작성

---
+++ src/main/java/study/msa/msadesigntasklecture/MsaDesignTaskLectureApplication.java
@@ -0,0 +1,13 @@
+package study.msa.msadesigntasklecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaDesignTaskLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaDesignTaskLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/msa/msadesigntasklecture/MsaDesignTaskLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package study.msa.msadesigntasklecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaDesignTaskLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
